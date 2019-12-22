ba09447f8a0c38dd1a852bd62fbdcdec1fe28b12
Daniil <zelenko.danil@gmail.com>
2019-12-22 14:35:05 +0200

    Made a router

--- components/index.js
+++ components/index.js
@@ -4,15 +4,31 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from "./Home";
 import AboutMe from "./AboutMe";
 import AboutSite from "./AboutSite";
+import Catalog from "./Catalog";
 
 function App() {
     return (
         <Router>
 
-            <Route exact to="/">
+            <Route exact path="/">
                 <Home />
             </Route>
-        
+
+            <Route exact path="/about-site">
+                <AboutSite />
+            </Route>
+
+            <Route exact path="/about-me">
+                <AboutMe />
+            </Route>
+
+            <Route exact path="/catalog">
+                <Catalog />
+            </Route>
+
+            <Route>
+                <h1>Error 404. Not found</h1>
+            </Route>
         </Router>
     )
 }
