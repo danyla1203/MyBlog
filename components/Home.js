6196d02081bc8459e3fa1af627fa2b0326c3eb98
Daniil <zelenko.danil@gmail.com>
2019-12-22 20:20:53 +0200

    Made a Home.js

--- components/Home.js
+++ components/Home.js
@@ -5,12 +5,34 @@ import ArticleList from "./articles/ArticleList";
 import RandomArticles from "./articles/RandomArticles";
 
 class Home extends Component {
-    
-    render() { 
+
+    constructor() {
+        super();
+
+        this.state = {
+            articles: "Nothing here" 
+        }
+    }
+
+    componentDidMount() {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "/data/articles");
+        xhr.send();
+
+        xhr.onload = () => {
+            let result = JSON.parse(xhr.response);
+            this.setState({ articles: result })
+        }
+    }
+
+    render() {
+        let articles = this.state.articles;
+        console.log(articles);
         return ( 
             <div id="wrapper">
                 <Header />
-                <ArticleList />
+                <ArticleList articles = { articles } />
+                <RandomArticles articles = { articles } />
             </div>
         );
     }
