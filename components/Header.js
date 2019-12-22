ed990c2e3d5e760d2f53627b123a4fce5c9400bc
Daniil <zelenko.danil@gmail.com>
2019-12-22 14:35:51 +0200

    Made a header

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Header() {
     return (
-       
+        <header>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about-site">About Site</a></li>
+                <li><a href="/about-me">About Me</a></li>
+                <li><a href="/catalog">Categories</a></li>
+            </ul>
+        </header>
     )
 }
 
