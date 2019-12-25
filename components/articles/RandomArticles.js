7c39f3840d44a7273b78795a00038c03bf58f11d
Daniil <zelenko.danil@gmail.com>
2019-12-25 19:01:48 +0200

    Made a RandomArticles

--- components/articles/RandomArticles.js
+++ components/articles/RandomArticles.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
-class RandomArticles extends Component {
-    render() { 
-        return ( 
 
-        );
+function RandomArticles(props) {
+
+    let renderArticles = (element) => {
+        return( 
+            <div key={ element.id }>
+                <h3>{element.title}</h3>
+                <Link to={"/post/" + element.id} >Link</Link>
+            </div>
+        )
     }
+    
+    let renderedArticles = props.articles;
+    if(Array.isArray(props.articles)) {
+        renderedArticles = props.articles.map((element) => {
+            return renderArticles(element);
+        })
+    }
+
+    
+    return (
+        <div>
+            { renderedArticles }
+        </div>
+    )
 }
- 
+
+
 export default RandomArticles;
