6e49ffc992494be850df28fb976e77b05c18239e
Daniil <zelenko.danil@gmail.com>
2019-12-26 18:11:34 +0200

    Rewrite a Router

--- components/index.js
+++ components/index.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from "./Home";
 import AboutMe from "./AboutMe";
 import AboutSite from "./AboutSite";
 import Catalog from "./Catalog";
+import FullArticle from './articles/FullArticle';
 
 function App() {
     return (
         <Router>
-
-            <Route exact path="/">
-                <Home />
-            </Route>
-
-            <Route exact path="/about-site">
-                <AboutSite />
-            </Route>
-
-            <Route exact path="/about-me">
-                <AboutMe />
-            </Route>
-
-            <Route exact path="/catalog">
-                <Catalog />
-            </Route>
-
-            <Route>
-                <h1>Error 404. Not found</h1>
-            </Route>
+            <Switch>
+                <Route exact path="/" component={ Home } />
+                
+                <Route exact path="/about-site" component={ AboutSite } />
+    
+                <Route exact path="/about-me" component={ AboutMe } />
+        
+                <Route exact path="/catalog" component={ Catalog } />
+            
+                <Route exact path="/post/:idPost" component={ FullArticle } />
+                    
+                <Route>
+                    <h1>Error 404. Not found</h1>
+                </Route>
+            </Switch>
         </Router>
     )
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
