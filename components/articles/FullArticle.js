80012c6f41b588d12bedd1419d19b61c6f9b201a
Daniil <zelenko.danil@gmail.com>
2019-12-26 19:21:01 +0200

    Made FullArticle

--- components/articles/FullArticle.js
+++ components/articles/FullArticle.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
-function FullArticle() {
+import Header from "../Header";
+
+
+function FullArticle(props) {
+
+    let getArticle = () => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "/data/articles/" + props.match.params.idPost, false);
+        xhr.send();
+
+        let result = JSON.parse(xhr.response);
+    
+        return result;
+    }
+    
+    let article = getArticle();
+
     return (
-        
+        <div>
+             <Header />
+            <div id="article">
+                <h3>{ article.title }</h3>
+                <p>{ article.text }</p>
+                <h4>{ article.date }</h4>
+            </div>
+        </div>
     )
 }
+
+export default FullArticle;
