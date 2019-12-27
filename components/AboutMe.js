5b4afbf03e690fd688d613f251653d428ab87d8b
Daniil <zelenko.danil@gmail.com>
2019-12-27 15:21:15 +0200

    Made a About pages

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Header from "./Header";
+
 function AboutMe() {
     return (
-
+        <div>
+            <Header />
+            <p>Hello, it's my blog. One shoot, one kill</p>
+        </div>
     )
 }
  
--- components/AboutSite.js
+++ components/AboutSite.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Header from "./Header";
+
 function AboutSite() {
     return (
-
+        <div>
+            <Header />
+            <p>
+                Empy page. Nothing here
+            </p>
+        </div>
     )
 }
  
-export default Article;
+export default AboutSite;
